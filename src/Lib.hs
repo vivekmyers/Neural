c009f5db619661200b4814ac29fa2392fdbcca42
Vivek Myers <vivek.myers@gmail.com>
2018-04-13 12:54:14 -0400

    Runnable

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( Net () 
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+newtype Net = Net {layers :: [Layer]}
+data Layer = Layer {weights :: [[Double]], biases :: [Double]}
+type Input = [Double]
+type Result = [Double]
+
+run :: Net -> Input -> Result
+run Net {layers = n} input = foldr ($$) input n
+  where (Layer w b) $$ i = zipWith ((sig .) . (+)) (sum . zipWith (*) i <$> w) b
+
+cost :: Net -> Input -> Result -> Double
+cost n input result = sum $ (**2) <$> zipWith (-) (run n input) result
+
+sig :: Double -> Double
+sig x = 1 / (1 + exp (-x))
+
